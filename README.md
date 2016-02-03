56c77bc1df50bbbee2972f00448de8309175be97
otacke <o.tacke@web.de>
2016-02-03 19:14:57 +0100

    added option to set an offset of +/- 1 week (not yet saved); now omits .class files in repository

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # timetray
 TimeTray is a very simple program that I originally hacked on one day for a former colleague of mine many years ago.
-It displays the calender week in a system tray - a feature that Windows still lacks in 2016. Since TimeTray is written in Java, it can run on other operating systems as well, e.g. Linux or MacOS.
+It displays the calender week in a system tray -- a feature that Windows still lacks in 2016. Since TimeTray is written in Java, it can run on other operating systems as well, e.g. Linux or MacOS.
 
 If you don't use the "Download ZIP" option but only want to download TimeTray.jar, don't right-click it in the list (!) but left-click on it and get the "RAW" version!
 
-Just make sure that you're running a Java Runtime Engine (e. g. the JRE from [Oracle](http://www.java.com/en/download/ "Oracle")), and put TimeTray.jar into your autostart folder, crontab, whatever...
+Just make sure that you're running a Java Runtime Environment (e. g. the JRE from [Oracle](http://www.java.com/en/download/ "Oracle")), and put TimeTray.jar into your autostart folder, crontab, whatever...
 
 ## to be done
-TimeTray is totally working - I hope ;-) I cannot test it on Windows because I don't use Windows. Anyway, allowing to set some parameters would be useful:
+TimeTray is totally working -- I hope ;-) I cannot test it on Windows because I don't use Windows. Anyway, allowing to set (and save) some parameters would be useful:
 
 * the tray icon's background color
 * the tray icon's font color
 * the tray icon's font
 * an optional offset of -1 or +1 if you're running a locale version of your OS that doesn't match your local calendar customs
 
-I might as well play with a wrapper like [launch4j](http://launch4j.sourceforge.net/ "launch4j") to create a Windows native executable, but that's not my priority. So, you might do that on your own as well ;-)
+I might as well play with a wrapper like [launch4j](http://launch4j.sourceforge.net/ "launch4j") to create a Windows native executable that includes a Runtime Environment, but that's not my priority and that would bloat the file massively.. So, you might do that on your own as well ;-)
 
 When will that be done? When it's done.
 
--- TimeTray.java
+++ TimeTray.java
@@ -5,18 +5,25 @@
  * showing the current calender week. Further information about date and time
  * can be obtained by hovering over the symbol with the mouse pointer.
  *
+ * TODO: open one presets window only
+ * TODO: repaint the TrayIcon when a new week begins
+ * TODO: load and save methods for the presets
+ * TODO: color chooser settings
+ * TODO: font settings (maybe)
+ * TODO: refactoring
+ *
  * @author Oliver Tacke, Armin Schöning
- * @version 1.1, Jan 2016
+ * @version 1.2, Feb 2016
  */
 
 //import neccessary Classes
 import java.awt.AWTException;
 import java.awt.AlphaComposite;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
@@ -33,22 +40,15 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JSlider;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class TimeTray extends TimerTask implements ActionListener {
-
-	// default background color for the tray icon
-	private final Color DEFAULT_BACKGROUND_COLOR =
-			new Color( 221, 221, 221, 0 );
-
-	// default font color for the tray icon
-	private final Color DEFAULT_FONT_COLOR = Color.white;
-
-	// in general, locale settings probably match the local customs
-	private final Integer DEFAULT_OFFSET = 0;
-
-	// default format for displaying date information
-	private final String DEFAULT_SDF_FORMAT =
-			"'week' w, EEEE, MMMM dd, yyyy, HH:mm";
 
 	// TrayIcon to be used in system tray
 	private TrayIcon trayIcon;
@@ -56,41 +56,34 @@ public class TimeTray extends TimerTask implements ActionListener {
 	// dimension of the TrayIcon
 	private Dimension iconSize;
 
-	// image to be shown within TrayIcon
-	private BufferedImage image;
-
 	// internal Java calendar for getting date information
 	private Calendar calendar;
 
-	// SimpleDateFormat for use in connection with the ToolTip text
-	private SimpleDateFormat sdf;
-
-	// background color for the tray icon
-	private Color backgroundColor;
-		
-	// font color for the tray icon
-	private Color fontColor;
-
-	// font to be used
-	private Font font;
-
-	/*
-	 * used to correct the calendar week by +1 or -1 if neccessary
-	 * 
-	 * this may come in handy, because Java determines the start of a week
-	 * depending on the locale settings of your operating system that can
-	 * differ from the customs of your region.
-	 */
-	private Integer offset;
-
 	// popupMenu to be used in connection with the TrayIcon
 	private PopupMenu menu = this.createMenu();
+		
+	// Presets for the TrayIcon
+	private Presets presets = null;
+
+	// indicate if the TrayIcon needs repainting
+	private boolean repaint = false;
+
+	/**
+	 * main method
+	 *
+	 * @param args arguments
+	 */
+	public static void main(String[] args) {
+		// create new TrayIcon if a SystemTray is supported by the OS
+		if ( SystemTray.isSupported() ) {
+			new TimeTray();
+		} else {
+			// TODO: Display a "Sorry" message
+		}
+	}
 
 	/**
 	 * TimeTray Constructor
-	 *
-	 * @author Oliver Tacke
-	 * @version 1.0, May 2007
 	 */
 	public TimeTray()  {
 
@@ -99,100 +92,86 @@ public class TimeTray extends TimerTask implements ActionListener {
 		iconSize = systemTray.getTrayIconSize();
 
 		// set presets
-		this.setPresets( iconSize.height );
+		presets  = new Presets( iconSize.height );
+		calendar = Calendar.getInstance();
 
-		// run thread and set timer to update every second
-		run();
-                
+		// create TrayIcon according to iconSize
+		trayIcon = new TrayIcon( getTrayImage(), "TimeTray", menu );
 		try {
 			systemTray.add( trayIcon );
 		} catch ( AWTException ex ) {
             ex.printStackTrace();
 		}
-              
+
+		// run thread and set timer to update every second (if neccessary)
+		run();
+                   
 		Timer timer = new Timer();
-		timer.schedule( this, 60000, 60000 );
+		timer.schedule( this, 1000, 1000 );
 	}
 
 	/**
 	 * run method
-	 *
-	 * @author Oliver Tacke, Armin Schöning
-	 * @version 1.1, January 2016
 	 */
-	
-	// TODO: refactor
 	public void run() {
-            
-            // create TrayIcon according to iconSize
-            image = new BufferedImage(
-					iconSize.width,
-					iconSize.height,
-					BufferedImage.TYPE_INT_ARGB );
-            trayIcon = new TrayIcon( image, "TimeTray", menu );
-            
+		if ( repaint ) {
             // get current date and set ToolTipText accordingly
-            calendar = Calendar.getInstance();
-            trayIcon.setToolTip( sdf.format( calendar.getTime() ) );
+            trayIcon.setToolTip( presets.sdf.format( calendar.getTime() ) );
+
+            // show drawn image
+            trayIcon.setImage( getTrayImage() );
+			
+			this.repaint = false;
+		}
+	}
+
+	/**
+	 * create the image for the TrayIcon
+	 *
+	 * @returns BufferedImage the image for the TrayIcon
+	 */
+	private BufferedImage getTrayImage() {
+			BufferedImage image = new BufferedImage(
+				this.iconSize.width,
+				this.iconSize.height,
+				BufferedImage.TYPE_INT_ARGB );
 
             // draw background image
             Graphics2D g2 = image.createGraphics();
-            g2.setColor( backgroundColor );
+            g2.setColor( presets.backgroundColor );
             g2.fillRect( 0, 0, iconSize.width, iconSize.height );
 
-			// font
+			// draw number
             g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING,
 					RenderingHints.VALUE_ANTIALIAS_ON ); 
-            g2.setColor( fontColor );
-            g2.setFont( font );
-            FontMetrics fm = g2.getFontMetrics( font );
+            g2.setColor( presets.fontColor );
+            g2.setFont( presets.font );
+            FontMetrics fm = g2.getFontMetrics( presets.font );
             int fontWidth = fm.stringWidth(
 					String.valueOf( calendar.get( Calendar.WEEK_OF_YEAR ) ) );
             g2.drawString(
 					String.valueOf(
-							calendar.get( Calendar.WEEK_OF_YEAR ) + offset ),
+							calendar.get( Calendar.WEEK_OF_YEAR ) + presets.offset ),
 					( iconSize.width-fontWidth ) / 2,
 					iconSize.height - 3 );
 
-            // show drawn image
-            trayIcon.setImage( image );
-
-            g2.dispose();
-	}
-
-	/**
-	 * main method
-	 *
-	 * @param args arguments
-	 *
-	 * @author Oliver Tacke
-	 * @version 1.0, May 2007
-	 */
-	public static void main(String[] args) {
-		// create new TrayIcon if a SystemTray is supported by the OS
-		if ( SystemTray.isSupported() ) {
-			new TimeTray();
-		}
+            return image;
 	}
 
 	/**
 	 * ActionListener for the TrayIcon
 	 *
 	 * @param e the event triggering the ActionListener
-	 *
-	 * @author Oliver Tacke
-	 * @version 1.0, May 2007
 	 */
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed( ActionEvent ev ) {
 		// user chose to exit TimeTray
-		if ( e.getActionCommand() == "quit" ) {
+		if ( ev.getActionCommand() == "quit" ) {
 			System.exit( 0 );
 		}
 		// user requests information about TimeTray
-		if ( e.getActionCommand() == "settings" ) {
-			// TODO: implement settings menu
-			trayIcon.displayMessage(
-					"Coming soon...", "Coming soon: settings for TimeTray!", TrayIcon.MessageType.INFO );
+		if ( ev.getActionCommand() == "settings" ) {
+			SettingsWindow settingsWindow = new SettingsWindow( this );
+			settingsWindow.setVisible( true );
 		}
 	}
 
@@ -200,9 +179,6 @@ public class TimeTray extends TimerTask implements ActionListener {
 	 * creates a PopUp menu for the TrayIcon
 	 *
 	 * @return PopUp menu for the TrayIcon
-	 *
-	 * @author Oliver Tacke
-	 * @version 1.0, May 2007
 	 */
 	private PopupMenu createMenu() {
 		PopupMenu menu = new PopupMenu( "TimeTray" );
@@ -218,7 +194,7 @@ public class TimeTray extends TimerTask implements ActionListener {
 		// exit item
 		MenuItem menuExit = new MenuItem( "Quit" );
 		menuExit.setActionCommand( "quit" );
-		menu.add(menuExit);
+		menu.add( menuExit );
 
 		menu.addActionListener( this );
 
@@ -226,27 +202,142 @@ public class TimeTray extends TimerTask implements ActionListener {
 	}
 	
 	/**
-	 * sets individual settings or sets default values (not yet)
+	 * individual settings or sets default values (not yet)
 	 *
-	 * @param fontHeight the height of the TrayIcon for determining the font
-	 *
-	 * @author Oliver Tacke
-	 * @version 1.0, Jan 2016
+	 * TODO: setters/getters
 	 */
-	private void setPresets( Integer trayHeight ) {
+	private class Presets {
+		// default background color for the tray icon
+		private final Color DEFAULT_BACKGROUND_COLOR =
+				new Color( 221, 221, 221, 0 );
+
+		// default font color for the tray icon
+		private final Color DEFAULT_FONT_COLOR = Color.white;
+
+		// in general, locale settings probably match the local customs
+		private static final int DEFAULT_OFFSET = 0;
+
+		// default format for displaying date information
+		private static final String DEFAULT_SDF_FORMAT =
+				"'week' w, EEEE, MMMM dd, yyyy, HH:mm";		
+		
+		// background color for the tray icon
+		private Color backgroundColor;
+			
+		// font color for the tray icon
+		private Color fontColor;
+
+		// font to be used
+		private Font font;
+
 		/*
-		 * I use "SansSerif" instead of "Arial" because this tool might run
-		 * anywhere, not only Windows
+		 * used to correct the calendar week by +1 or -1 if neccessary
+		 * 
+		 * this may come in handy, because Java determines the start of a week
+		 * depending on the locale settings of your operating system that can
+		 * differ from the customs of your region.
 		 */
-		final Font DEFAULT_FONT =
-				new Font( "SansSerif", Font.PLAIN, trayHeight );
+		private int offset;
+
+		// SimpleDateFormat for use in connection with the ToolTip text
+		private SimpleDateFormat sdf;
 		
-		backgroundColor = DEFAULT_BACKGROUND_COLOR;
-		fontColor       = DEFAULT_FONT_COLOR;
-		font            = DEFAULT_FONT;
-		offset          = DEFAULT_OFFSET;
-		sdf				= new SimpleDateFormat( DEFAULT_SDF_FORMAT );
+		private Presets( int trayHeight ) {
+			/*
+			 * I use "SansSerif" instead of "Arial" because this tool might run
+			 * anywhere, not only Windows
+			 */
+			final Font DEFAULT_FONT =
+					new Font( "SansSerif", Font.PLAIN, trayHeight );
+			
+			backgroundColor = DEFAULT_BACKGROUND_COLOR;
+			fontColor       = DEFAULT_FONT_COLOR;
+			font            = DEFAULT_FONT;
+			offset          = DEFAULT_OFFSET;
+			sdf				= new SimpleDateFormat( DEFAULT_SDF_FORMAT );			
+		}
+
+		/**
+		 * Load presets (from a JSON file)
+		 *
+		 * @returns Presets the presets
+		 */
+		private Presets loadPresets() {
+			return null;
+		}
+
+		/**
+		 * Save presets (to a JSON file)
+		 */		
+		private void savePresets() {
+		}
 		
-		// TODO: implement loading settings
-	}	
+	}
+
+	/**
+	 * Window for changing the settings
+	 */	
+	private class SettingsWindow extends JFrame implements ChangeListener {
+		// Tray
+		private TimeTray parent;		
+		
+		// Slider for setting the offset
+		private JSlider offsetSlider;
+
+		/**
+		 * Constructor for SettingsWindow
+		 *
+		 * @param TimeTray parent Tray opening the window
+		 */		
+		private SettingsWindow( TimeTray parent ) {
+			this.parent = parent;
+			this.getContentPane().setLayout( new BoxLayout( this.getContentPane(), BoxLayout.PAGE_AXIS ) );
+			this.initWindow();
+		}
+		
+		/**
+		 * Build the window
+		 */
+		private void initWindow() {
+			JLabel sliderLabel = new JLabel( "Offset (not saved yet)" );
+			sliderLabel.setAlignmentX( Component.LEFT_ALIGNMENT );
+			
+			offsetSlider = createOffsetSlider( this );
+
+			this.setTitle( "Presets (not finished)" );
+			this.getContentPane().add( sliderLabel );
+			this.getContentPane().add( Box.createRigidArea( new Dimension( 0, 5 ) ) );
+			this.getContentPane().add( offsetSlider );
+			this.pack();
+		}
+		
+		/**
+		 * Build the slider for setting the offset
+		 *
+		 * @param SettingsWindow window the window that implements the Change Listener
+		 * @return JSlider the slider for setting the offset
+		 */		
+		private JSlider createOffsetSlider( SettingsWindow window ) {
+			JSlider offsetSlider = new JSlider( JSlider.HORIZONTAL, -1, 1, window.parent.presets.offset );
+			offsetSlider.setAlignmentX( Component.LEFT_ALIGNMENT );
+			offsetSlider.setMajorTickSpacing( 1 );
+			offsetSlider.setPaintTicks( true );
+			offsetSlider.addChangeListener( window );
+			offsetSlider.setPaintLabels( true );
+			
+			return offsetSlider;
+		}
+
+		/**
+		 * register change events in the window
+		 *
+		 * @param ChangeEvent an event
+		 */
+		public void stateChanged( ChangeEvent ev ) {
+			JSlider source = (JSlider)ev.getSource();
+			this.parent.presets.offset = source.getValue();
+			this.parent.repaint = true;
+		}
+		
+	}
 }
